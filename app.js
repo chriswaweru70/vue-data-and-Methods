193ce98d3f5f12199f4bc7c56007801a1615de76
Chris <chris@pop-os.localdomain>
2019-06-30 08:17:31 +0300

    Data binding

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ new Vue({
     el: "#vue-app",
     data: {
         name: 'Chris',
-        job: 'Software Developer'
+        job: 'Software Developer',
+        website: "https://www.youtube.com/watch?v=xIOwFTCBBDg&list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa&index=5",
+        websiteTag: '<a href="https://www.youtube.com/watch?v=xIOwFTCBBDg&list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa&index=5">THe Net ninja</a>',
+        rawhtml: '<span style="color:purple"></span>'
     },
     methods: {
         greeting: function (event) {
